eda4f41b2a32abf48a5c5d91b3138508a025dd08
Andrea McIntosh <akmcinto@ualberta.ca>
2016-02-18 00:20:25 -0700

    Working?

--- a2_draft.lisp
+++ a2_draft.lisp
@@ -1,38 +1,22 @@
 (defun fl-interp-atom-context (E C)
     (cond
-        (
-            (null C)
-            E
-        )
-        (
-            (null (caar C))
-            (fl-interp-atom-context E (cdr C))
-        )
+        ((null C) E)
         (
             (equal E (caar C))
-            (cadar C)
+            (cdar C)
         )
         (
             T
-            (fl-interp-atom-context E (cons (cons (cdaar C) (cddar C)) (cdr C)))
+            (fl-interp-atom-context E (cdr C))
         )
     )
 )
 
 (defun fl-interp-atom (E P C)
     (cond
-        (
-            (null P)
-            (fl-interp-atom-context E C)
-        )
-        (
-            (equal (caar P) E)
-            (list (get_fnargs (cdar P)) (get_fnbody (car P)))
-        )
-        (
-            T
-            (fl-interp-atom E (cdr P) C)
-        )
+        ((null P) (fl-interp-atom-context E C))
+        ((equal (caar P) E) (list (get_fnargs (cdar P)) (get_fnbody (car P))))
+        (t (fl-interp-atom E (cdr P) C))
     )
 )
 
@@ -55,52 +39,54 @@
 )
 
 (defun _fl-interp (E P C)
-    (if (atom E)  ; this includes the case where expr is nil
-        (fl-interp-atom E P C)
-        (let
-            (
-                (f (car E))
-                (arg (cdr E))
-            )
-            (cond
-                ; handle built-in functions
-                ((eq f 'if) (if (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C) (_fl-interp (caddr arg) P C)))
-                ((eq f 'null)  (null (_fl-interp (car arg) P C)))
-                ((eq f 'atom)  (atom (_fl-interp (car arg) P C)))
-                ((eq f 'eq)  (eq (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f 'first)  (car (_fl-interp (car arg) P C)))
-                ((eq f 'rest)  (cdr (_fl-interp (car arg) P C)))
-                ((eq f 'cons) (cons (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f 'equal)  (equal (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f 'number)  (numberp (_fl-interp (car arg) P C)))
-                ((eq f '+)  (+ (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f '-)  (- (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f '*)  (* (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f '>)  (> (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f '<)  (< (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f '=)  (= (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
-                ((eq f 'and)  (not (null (and (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))))
-                ((eq f 'or)  (not (null (or (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))))
-                ((eq f 'not)  (not (_fl-interp (car arg) P C)))
+    (cond
+        ((atom E) (fl-interp-atom E P C))  ; this includes the case where expr is nil
+        ;((numberp (car E)) E)
+        (t
+            (let ((f (car E)) (arg (cdr E)))
+                (cond
+                    ; handle built-in functions
+                    ((eq f 'if) (if (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C) (_fl-interp (caddr arg) P C)))
+                    ((eq f 'null)  (null (_fl-interp (car arg) P C)))
+                    ((eq f 'atom)  (atom (_fl-interp (car arg) P C)))
+                    ((eq f 'eq)  (eq (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f 'first)  (car (_fl-interp (car arg) P C)))
+                    ((eq f 'rest)  (cdr (_fl-interp (car arg) P C)))
+                    ((eq f 'cons) (cons (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f 'equal)  (equal (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f 'number)  (numberp (_fl-interp (car arg) P C)))
+                    ((eq f '+)  (+ (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f '-)  (- (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f '*)  (* (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f '>)  (> (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f '<)  (< (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f '=)  (= (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))
+                    ((eq f 'and)  (not (null (and (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))))
+                    ((eq f 'or)  (not (null (or (_fl-interp (car arg) P C) (_fl-interp (cadr arg) P C)))))
+                    ((eq f 'not)  (not (_fl-interp (car arg) P C)))
+                    ((equal (fl-interp-atom f P C) f) E)
 
-                (
-                    T
-                    (let
-                        (
-                            (ev_args (eval_args arg P C))
-                            (closure (_fl-interp f P C))
-                        )
+                    (t
                         (let
-                            (
-                                (new_context (cons (cons (caar closure) ev_args) C))
-                                (body (cadr closure))
+                            ((ev_args (eval_args arg P C))
+                                (closure (_fl-interp f P C)))
+                            (let
+                                ((new_context (get_context (car closure) ev_args C))
+                                    (body (cadr closure)))
+                                (_fl-interp body P new_context)
                             )
-                            (_fl-interp body P new_context)
                         )
                     )
                 )
             )
         )
+    )
+)
+
+(defun get_context (vars nums C)
+    (cond
+        ((null nums) C)
+        (t (cons (cons (car vars) (car nums)) (get_context (cdr vars) (cdr nums) C)))
     )
 )
 

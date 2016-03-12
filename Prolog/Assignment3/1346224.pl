651428a5fc609b51f2d8e5c65868ed1262a80331
Andrea McIntosh <akmcinto@ualberta.ca>
2016-03-12 11:48:02 -0700

    More testing!

--- Prolog/Assignment3/1346224.pl
+++ Prolog/Assignment3/1346224.pl
@@ -10,9 +10,9 @@ xreverse([F|L1], L2) :- xreverse(L1, N), append(N, [F], L2).
 /* --------------------
 #2
 ---------------------*/
-xunique([], _).
-xunique([F|L], Lu) :- member(F, Lu), xunique(L, Lu).
-xunique([F|L], Lu) :- notMember(F, Lu), append(Lu, [F], X), xunique(L, X).
+xunique([], []).
+xunique([F|L], Lu) :- member(F, L), xunique(L, Lu).
+xunique([F|L], [F|Lu]) :- notMember(F, L), xunique(L, Lu).
 
 notMember(_, []).
 notMember(A, [F|R]) :- A \== F, notMember(A, R).

58060d75de5a1fcf379a13853565217427db9230
Andrea McIntosh <akmcinto@ualberta.ca>
2016-03-14 15:57:35 -0600

    Moving on to clique

--- Prolog/Assignment3/1346224.pl
+++ Prolog/Assignment3/1346224.pl
@@ -1,8 +1,5 @@
 /* --------------------
 #1
-xreverse(L, R) :- xreverse(L, [], R).
-xreverse([], R, R).
-xreverse([H|T], R, A) :- xreverse(T, [H|R], A).
 ---------------------*/
 xreverse([], []).
 xreverse([F|L1], L2) :- xreverse(L1, N), append(N, [F], L2).
@@ -21,5 +18,38 @@ notMember(A, [F|R]) :- A \== F, notMember(A, R).
 /* --------------------
 #3
 ---------------------*/
-xunion([],[],[]).
 xunion(L1, L2, L) :- append(L1, L2, X), xunique(X, L).
+
+/* --------------------
+#4
+---------------------*/
+removeLast([F|L], L, F) :- L = [].
+removeLast([F|L], [F|L1], Last) :- removeLast(L, L1, Last).
+
+/* --------------------
+#5
+---------------------*/
+clique(L) :- findall(X,node(X),Nodes),
+             subset(L,Nodes), allConnected(L).
+subset([], _).
+subset([X|Xs], Set) :- xappend(_, [X|Set1], Set),
+subset(Xs, Set1).
+
+xappend([], L, L).
+xappend([H|T], L, [H|R]) :- xappend(T, L, R).
+%5.1
+
+
+
+
+node(a).
+node(b).
+node(c).
+node(d).
+node(e).
+
+edge(a,b).
+edge(b,c).
+edge(c,a).
+edge(d,a).
+edge(a,e).
